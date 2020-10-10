e45bd4312d660747aacfd40c16b6c9a2894d6651
MarynaPR <pryadkamaryna@gmail.com>
2020-10-10 16:19:24 -0500

    fetch the weather function

--- weather.js
+++ weather.js
@@ -1,14 +1,16 @@
 // preudocode 
 
+var searchCity = document.querySelector("#searchcity");
 //1.fuction for get user inout for the city
-var searchTravelCity = function () {
-    //event.preventDerault();
+var searchTravelCity = function (event) {
+    event.preventDerault();
     console.log("city");
-    var searchCity = document.querySelector("#searchCity").value;
+    var city = searchCity.value.trim();
     //storeTravelCity(searchCity);
     // to make sure user writes a city
-    if (searchCity) {
+    if (city) {
         storeTravelCity(searchCity);
+        weatherCall(searchCity);
     } else {
         alert("Please select a city!");
     }
@@ -16,9 +18,14 @@ var searchTravelCity = function () {
     //     name: searchCity,
     // };
 }
+var displayCity = function () {
+
+}
+var list = []
 //2 function-create a list of cities and storing the input
-var storeTravelCity = function (searchCity) {
+var storeTravelCity = function (city) {
     console.log("storedCities");
+    localStorage.setItem("list", JSON.stringify(list));
     var listOfCities = document.createElement("li");
     listOfCities.classList.add("searchCity")//("list-group-city")//, "list-group-stored");
     //create list -div to hold city info and add to list them
@@ -33,33 +40,31 @@ var storeTravelCity = function (searchCity) {
         if (event.this.city == "li") {
         }
         alert("hello")
-
     }
     storedCities.appendChild(listOfCities);
+    document.querySelector("#searchBtn").addEventListener("click", searchTravelCity)
 };
 
-document.querySelector("#searchBtn").addEventListener("click", searchTravelCity);
 ///3 .f for weather for today and conditions in the chosen city
 
-var weatherCall = function (searchCity) {
+var weatherCall = function (city) {
     console.log("weather")
+    //var searchCity = document.querySelector("#searchCity").value;
     //fetch the weather with api keys
     fetch(
         "http://api.openweathermap.org/data/2.5/weather?q="
         + searchCity
-        + "&appid=b76c30386bab576d023d70f50d7d35cb"
+        + "&appid=b76c30386bab576d023d70f50d7d35cb&units=imperial"
     )
         .then(function (response) {
+            //console.log(response);
             return response.json();
         })
-        .then(function (data) {
-            console.log(data);
-            // })
-            // .catch.function()
-            // Use 'querySelector' to get the ID of where the GIF will be displayed
-            var responseContainer
+        .then(function (response) {
+            console.log(response.data);
 
-
-        }
-
+        })
+}
 //4 f for forecast display
+
+document.querySelector("#searchBtn").addEventListener("submit", searchTravelCity);
