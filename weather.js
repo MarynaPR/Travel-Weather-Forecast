dfbe521e6c47cfb8b0922019d5e66732fb103208
MarynaPR <pryadkamaryna@gmail.com>
2020-10-11 12:17:16 -0500

    5 day forecast

--- weather.js
+++ weather.js
@@ -108,43 +108,50 @@ var getForecast = function (city) {
             forecastContainer.className = "\'row\'";
             forecast.innerHTML = "<h3 class=\"mt-3\">5-Day Forecast:</h3>";
 
-            // var for weather conditions displayed in the bootstrap card
+            // for loop 
+            for (var i = 0; i < 5; i++) {
 
-            var date = document.createElement("h3");
-            var body = document.createElement("div");
-            var cont = document.createElement("div");
-            var card = document.createElement("div");
-            var wind = document.createElement("p");
-            var temp = document.createElement("p");
-            var hum = document.createElement("p");
-            var uv = document.createElement("p");
-            var img = document.createElement("img");
+                // var for weather conditions displayed in the bootstrap card
 
-            // add class
-            date.classList.add("card-text");
-            body.classList.add("card-body", "p-2");
-            cont.classList.add("col-md-2");
-            card.classList.add("card,text-white");
-            wind.classList.add("card-text");
-            temp.classList.add("card-text");
-            hum.classList.add("card-text");
-            uv.classList.add("card-text");
-            img.classList.add("card-tex");//image?
+                var date = document.createElement("h3");
+                var body = document.createElement("div");
+                var cont = document.createElement("div");
+                var card = document.createElement("div");
+                var wind = document.createElement("p");
+                var temp = document.createElement("p");
+                var hum = document.createElement("p");
+                var uv = document.createElement("p");
+                var img = document.createElement("img");
 
-            //display
-            wind.textContent = "Wind: " + data.list[i].wind.speed + "MPH";
+                // add class
+                date.classList.add("card-text");
+                body.classList.add("card-body", "p-2");
+                cont.classList.add("col-md-2");
+                card.classList.add("card,text-white");
+                wind.classList.add("card-text");
+                temp.classList.add("card-text");
+                hum.classList.add("card-text");
+                uv.classList.add("card-text");
+                img.classList.add("card-tex");//image?
 
-            //connect by appending
-            cont.appendChild(card);
-            forecast.appendChild(cont);
-            body.appendChild(date);
-            body.appendChild(wind);
-            body.appendChild(temp);
-            body.appendChild(hum);
-            body.appendChild(uv);
-            body.appendChild(img);
-            card.appendChild(body);
+                //display
+                wind.textContent = "Wind: " + data.list[i].wind.speed + "MPH";
+                temp.textContent = "Temperature: " + data.list[i].main.temp + "F";
+                hum.textContent = "Humidity: " + data.list[i].main.humidity + "%";
+                img.textContent = "Icon: " + data.list[i].weather.icon + "";
+                uv.textContent = "Coord: " + data.list[i].coord.lon;
 
+                //connect by appending
+                cont.appendChild(card);
+                forecast.appendChild(cont);
+                body.appendChild(date);
+                body.appendChild(wind);
+                body.appendChild(temp);
+                body.appendChild(hum);
+                body.appendChild(uv);
+                body.appendChild(img);
+                card.appendChild(body);
+            }
         })
     // city name
     // date, 
